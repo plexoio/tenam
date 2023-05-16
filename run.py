09976bb5013456b963cb7c684e181eccbb9f206d
Frank Reinaldo Arellano Andrade <plexonft@gmail.com>
2023-05-16 16:21:41 +0000

    move files to modules directory, changes on run.py

--- run.py
+++ run.py
@@ -1,9 +1,9 @@
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
-from google_sheets import * # third-party
-from portfolio import Portfolio, Google_Sheet # author's
-from account import User, Asset, Taxation # author's
-from data_analysis import Data_Analysis # author's
-from transaction import Transaction # author's
+from modules.google_sheets import * # third-party
+from modules.portfolio import Portfolio, Google_Sheet # author's
+from modules.account import User, Asset, Taxation # author's
+from modules.data_analysis import Data_Analysis # author's
+from modules.transaction import Transaction # author's
 
 user = SHEET.worksheet('user')
 data = user.get_all_values()

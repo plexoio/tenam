adccda56c4ac8d51dcdfbe7ecf98f5210d4a4599
Frank Reinaldo Arellano Andrade <plexonft@gmail.com>
2023-05-19 19:48:04 +0000

    fix: try new os.system command for clearing on Heroku

--- modules/utilities.py
+++ modules/utilities.py
@@ -5,4 +5,4 @@ def clear_screen():
     '''
     Used all around the code to clear console when needed to avoid overcrowding
     '''
-    os.system('cls' if os.name == 'nt' else 'clear')
+    os.system('clear')

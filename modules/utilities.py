77cd0695931a95f4072d501d3b4eee409e2d9a98
Frank Reinaldo Arellano Andrade <plexonft@gmail.com>
2023-05-19 19:58:18 +0000

    fix: try again Heroky clear issue

--- modules/utilities.py
+++ modules/utilities.py
@@ -5,4 +5,8 @@ def clear_screen():
     '''
     Used all around the code to clear console when needed to avoid overcrowding
     '''
-    os.system('clear')
+    # Clear console for Windows, Linux, and macOS
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+    # Move the cursor to the top left corner of the console
+    print('\033[1;1H', end='')

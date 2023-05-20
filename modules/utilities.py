b6113e51bb246f51ad0fc02dcec5902010ea138c
Frank Reinaldo Arellano Andrade <plexonft@gmail.com>
2023-05-20 15:58:43 +0000

    fix: try time.sleep function could be the answer

--- modules/utilities.py
+++ modules/utilities.py
@@ -6,5 +6,5 @@ def clear_screen():
     Used all around the code to clear console when needed to avoid overcrowding
     '''
     # Clear console for Windows, Linux, and macOS
-    print('\n' * 100)
+    print('\n' * 20)
     os.system('cls' if os.name == 'nt' else 'clear')

4606d178e78645bed2d94723eb88ba9a503e530b
Frank Reinaldo Arellano Andrade <plexonft@gmail.com>
2023-05-20 19:50:17 +0000

    default.js back to normal

--- controllers/default.js
+++ controllers/default.js
@@ -1,12 +1,12 @@
-import { spawn } from 'node-pty';
-import { writeFile } from 'fs';
+const Pty = require('node-pty');
+const fs = require('fs');
 
-export function install () {
+exports.install = function () {
 
     ROUTE('/');
     WEBSOCKET('/', socket, ['raw']);
 
-}
+};
 
 function socket() {
 
@@ -16,7 +16,7 @@ function socket() {
     this.on('open', function (client) {
 
         // Spawn terminal
-        client.tty = spawn('python3', ['run.py'], {
+        client.tty = Pty.spawn('python3', ['run.py'], {
             name: 'xterm-color',
             cols: 80,
             rows: 24,
@@ -51,7 +51,7 @@ function socket() {
 
 if (process.env.CREDS != null) {
     console.log("Creating creds.json file.");
-    writeFile('creds.json', process.env.CREDS, 'utf8', function (err) {
+    fs.writeFile('creds.json', process.env.CREDS, 'utf8', function (err) {
         if (err) {
             console.log('Error writing file: ', err);
             socket.emit("console_output", "Error saving credentials: " + err);
